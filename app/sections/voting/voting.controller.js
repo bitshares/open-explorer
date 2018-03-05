ecbc9bb5aba88de2bc9d1602339e4b2ce881516f
oxarbitrage <oxarbitrage@gmail.com>
2018-03-05 14:47:44 -0800

    add worker title to voting page

--- app/sections/voting/voting.controller.js
+++ app/sections/voting/voting.controller.js
@@ -51,19 +51,19 @@
                 var workers = [];
                 var counter = 1;
                 angular.forEach(response2.data, function (value, key) {
-                    var parsed = {id: value[1], worker_account_name: value[0], proxy1: value[2].split(":")[1]
-                        , proxy2: value[3].split(":")[1], proxy3: value[4].split(":")[1], proxy4: value[5].split(":")[1], proxy5: value[6].split(":")[1], proxy6: value[7].split(":")[1]
-                        , proxy7: value[8].split(":")[1], proxy8: value[9].split(":")[1], proxy9: value[10].split(":")[1], proxy10: value[11].split(":")[1],
-                        tclass1: ((value[2].split(":")[1] == "Y") ? "success" : "danger"),
-                        tclass2: ((value[3].split(":")[1] == "Y") ? "success" : "danger"),
-                        tclass3: ((value[4].split(":")[1] == "Y") ? "success" : "danger"),
-                        tclass4: ((value[5].split(":")[1] == "Y") ? "success" : "danger"),
-                        tclass5: ((value[6].split(":")[1] == "Y") ? "success" : "danger"),
-                        tclass6: ((value[7].split(":")[1] == "Y") ? "success" : "danger"),
-                        tclass7: ((value[8].split(":")[1] == "Y") ? "success" : "danger"),
-                        tclass8: ((value[9].split(":")[1] == "Y") ? "success" : "danger"),
-                        tclass9: ((value[10].split(":")[1] == "Y") ? "success" : "danger"),
-                        tclass10: ((value[11].split(":")[1] == "Y") ? "success" : "danger")};
+                    var parsed = {id: value[1], worker_account_name: value[0], worker_name: value[2], proxy1: value[3].split(":")[1]
+                        , proxy2: value[4].split(":")[1], proxy3: value[5].split(":")[1], proxy4: value[6].split(":")[1], proxy5: value[7].split(":")[1], proxy6: value[8].split(":")[1]
+                        , proxy7: value[9].split(":")[1], proxy8: value[10].split(":")[1], proxy9: value[11].split(":")[1], proxy10: value[12].split(":")[1],
+                        tclass1: ((value[3].split(":")[1] == "Y") ? "success" : "danger"),
+                        tclass2: ((value[4].split(":")[1] == "Y") ? "success" : "danger"),
+                        tclass3: ((value[5].split(":")[1] == "Y") ? "success" : "danger"),
+                        tclass4: ((value[6].split(":")[1] == "Y") ? "success" : "danger"),
+                        tclass5: ((value[7].split(":")[1] == "Y") ? "success" : "danger"),
+                        tclass6: ((value[8].split(":")[1] == "Y") ? "success" : "danger"),
+                        tclass7: ((value[9].split(":")[1] == "Y") ? "success" : "danger"),
+                        tclass8: ((value[10].split(":")[1] == "Y") ? "success" : "danger"),
+                        tclass9: ((value[11].split(":")[1] == "Y") ? "success" : "danger"),
+                        tclass10: ((value[12].split(":")[1] == "Y") ? "success" : "danger")};
                     workers.push(parsed);
                     counter++;
                 });
