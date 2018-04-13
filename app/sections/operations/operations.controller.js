6a48da7845c292232222934e9080ccbb8930fecd
oxarbitrage <oxarbitrage@gmail.com>
2018-04-13 12:22:47 -0700

    add operation time

--- app/sections/operations/operations.controller.js
+++ app/sections/operations/operations.controller.js
@@ -7,6 +7,7 @@
     function operationsCtrl($scope, $filter, $routeParams, $http, appConfig, utilities) {
 
         var block_num = "";
+        var block_time = "";
         var virtual_op = "";
         var trx_in_block = "";
         var op_in_trx = "";
@@ -18,6 +19,7 @@
             .then(function(response) {
                 //console.log(response.data[0]);
                 block_num = response.data[0].block_num;
+                block_time = response.data[0].block_time;
                 virtual_op = response.data[0].virtual_op;
                 trx_in_block = response.data[0].trx_in_block;
                 op_in_trx = response.data[0].op_in_trx;
@@ -29,7 +31,7 @@
 
                 var operation_text = "";
                 operation_text = utilities.opText(appConfig, $http, type, raw_obj, function(returnData) {
-                    $scope.data = { name: name , block_num: block_num, virtual_op: virtual_op, trx_in_block: trx_in_block, op_in_trx: op_in_trx , result: result, type: op_type[0], color: op_type[1], raw: raw_obj, operation_text: returnData};
+                    $scope.data = { name: name , block_num: block_num, virtual_op: virtual_op, trx_in_block: trx_in_block, op_in_trx: op_in_trx , result: result, type: op_type[0], color: op_type[1], raw: raw_obj, operation_text: returnData, block_time: block_time};
 
                 });
             });
