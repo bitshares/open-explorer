2f72d1aa3260e9e682d0fed1b317d1aacce445be
oxarbitrage <oxarbitrage@gmail.com>
2018-08-05 05:12:56 -0700

    isorting in workers

--- app/sections/workers/workers.controller.js
+++ app/sections/workers/workers.controller.js
@@ -8,11 +8,12 @@
 
         $http.get(appConfig.urls.python_backend + "/get_workers")
             .then(function(response) {
-                //console.log(response.data);
                 var workers_current = [];
                 var workers_expired = [];
                 var start, end, now;
                 for(var i = 0; i < response.data.length; i++) {
+
+
                     var now_d = new Date();
                     var start_d = new Date(response.data[i][0].work_begin_date);
                     var end_d = new Date(response.data[i][0].work_end_date);
@@ -21,34 +22,65 @@
                     now = now_d.toLocaleString();
                     var start_date = start.split(" ", 1);
                     var end_date = end.split(" ", 1);
+
                     var votes_for = utilities.formatBalance(response.data[i][0].total_votes_for, 5);
                     var daily_pay = utilities.formatBalance(response.data[i][0].daily_pay, 5);
                     var tclass = "";
 
+                    var parsed;
+
                     var have_url = 0;
-                    if(response.data[i][0].url && response.data[i][0].url != "http://")
+                    if(response.data[i][0].url && response.data[i][0].url !== "http://") {
                         have_url = 1;
+                    }
 
                     if(now_d > end_d) {
                         tclass = "danger";
-                        var parsed = { name: response.data[i][0].name, daily_pay: daily_pay, url: response.data[i][0].url, have_url: have_url, votes_for: votes_for, votes_against: response.data[i][0].total_votes_against, worker: response.data[i][0].worker_account, start: start_date[0], end: end_date[0], id: response.data[i][0].id, worker_name: response.data[i][0].worker_account_name, tclass: tclass, perc: response.data[i][0].perc };
+                        parsed = { name: response.data[i][0].name,
+                                   daily_pay: daily_pay, url: response.data[i][0].url,
+                                   have_url: have_url,
+                                   votes_for: votes_for,
+                                   votes_against: response.data[i][0].total_votes_against,
+                                   worker: response.data[i][0].worker_account,
+                                   start: start_date[0], end: end_date[0],
+                                   id: response.data[i][0].id,
+                                   worker_name: response.data[i][0].worker_account_name,
+                                   tclass: tclass, perc: response.data[i][0].perc
+                        };
                         workers_expired.push(parsed);
                     }
                     else {
                         var voting_now = "";
                         if(now_d > start_d) {
 
-                            if(response.data[i][0].perc >= 50 && response.data[i][0].perc < 100)
+                            if(response.data[i][0].perc >= 50 && response.data[i][0].perc < 100) {
                                 tclass = "warning";
-                            else if(response.data[i][0].perc >= 100)
+                            }
+                            else if(response.data[i][0].perc >= 100) {
                                 tclass = "success";
+                            }
                         }
                         else {
                             tclass = "";
-                            if(start_d > now_d)
+                            if(start_d > now_d) {
                                 voting_now = "VOTING NOW!";
+                            }
                         }
-                        var parsed = { name: response.data[i][0].name, daily_pay: daily_pay, url: response.data[i][0].url, have_url: have_url, votes_for: votes_for, votes_against: response.data[i][0].total_votes_against, worker: response.data[i][0].worker_account, start: start_date[0], end: end_date[0], id: response.data[i][0].id, worker_name: response.data[i][0].worker_account_name, tclass: tclass, perc: response.data[i][0].perc, voting_now: voting_now };
+                        parsed = { name: response.data[i][0].name,
+                                   daily_pay: daily_pay,
+                                   url: response.data[i][0].url,
+                                   have_url: have_url,
+                                   votes_for: votes_for,
+                                   votes_against: response.data[i][0].total_votes_against,
+                                   worker: response.data[i][0].worker_account,
+                                   start: start_date[0],
+                                   end: end_date[0],
+                                   id: response.data[i][0].id,
+                                   worker_name: response.data[i][0].worker_account_name,
+                                   tclass: tclass,
+                                   perc: response.data[i][0].perc,
+                                   voting_now: voting_now
+                        };
                         workers_current.push(parsed);
                     }
                 }
@@ -56,6 +88,12 @@
                 $scope.workers_expired = workers_expired;
             });
 
+
+        utilities.columnsort($scope, "start", "sortColumn", "sortClass", "reverse", "reverseclass", "column");
+        utilities.columnsort($scope, "start", "sortColumn2", "sortClass2", "reverse2", "reverseclass2", "column2");
+
+
+        /*
         // table 1
         // column to sort
         $scope.column = 'start';
@@ -112,6 +150,7 @@
                 return '';
             }
         }
+        */
     }
 
 
