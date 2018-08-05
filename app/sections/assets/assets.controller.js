56a3b63edcf7a02fb11da01dd6aeeddae82ebad3
oxarbitrage <oxarbitrage@gmail.com>
2018-08-05 04:47:39 -0700

    sorting in committee member and asset pages

--- app/sections/assets/assets.controller.js
+++ app/sections/assets/assets.controller.js
@@ -6,10 +6,6 @@
 
     function assetsCtrl($scope, $filter, $routeParams, $location, $http, orderByFilter, appConfig, utilities) {
 
-        //$scope.scrollTo = function(id) {
-        //    $location.hash(id);
-        //    $anchorScroll();
-        //}
 		var path = $location.path();
 		var name = $routeParams.name;
 		if(name) {
@@ -24,11 +20,9 @@
                 //var raw = "";
                 $http.get(appConfig.urls.python_backend + "/get_asset_and_volume?asset_id=" + name)
                     .then(function(response) {
-                        //console.log(response.data[0]);
-                        //console.log(response.data[0].options.description);
                         var type;
                         var description;
-                        if(response.data[0].issuer == "1.2.0") {
+                        if(response.data[0].issuer === "1.2.0") {
                             description = response.data[0].options.description;
                             type = "SmartCoin";
                         }
@@ -37,22 +31,20 @@
                             description = description_p[3];
                             type = "User Issued";
                         }
-                        if(response.data[0].symbol == "BTS")
+                        if(response.data[0].symbol === "BTS") {
                             type = "Core Token";
+                        }
 
                         var long_description = false;
-                        //console.log(description.length);
                         try {
-                            if (description.length > 100)
+                            if (description.length > 100) {
                                 long_description = true;
+                            }
                         }
                         catch(err) { }
                         $http.get(appConfig.urls.python_backend + "/get_asset_holders_count?asset_id=" + name)
                             .then(function(response2) {
-                                //console.log(response2);
-
                                 var name_lower = response.data[0].symbol.replace("OPEN.", "").toLowerCase();
-
                                 var url = "images/asset-symbols/"+name_lower+".png";
                                 var image_url = "";
 
@@ -82,10 +74,12 @@
                                         market_cap: response.data[0].mcap/100000
                                     };
                                 }, function errorCallback(response3) {
-                                    if(type == "SmartCoin")
+                                    if(type === "SmartCoin") {
                                         image_url = "images/asset-symbols/white.png";
-                                    else
+                                    }
+                                    else {
                                         image_url = "images/asset-symbols/white.png";
+                                    }
 
                                     $scope.data = {
                                         symbol: response.data[0].symbol,
@@ -112,9 +106,7 @@
                             var markets = [];
                             $http.get(appConfig.urls.python_backend + "/get_markets?asset_id=" + name)
                                 .then(function(response) {
-                                    //console.log(response.data);
                                     angular.forEach(response.data, function(value, key) {
-                                        //console.log(value);
                                         var parsed = {pair: value[1], price: value[3], volume: value[4]};
                                         markets.push(parsed);
                                     });
@@ -123,10 +115,7 @@
                             var accounts = [];
                             $http.get(appConfig.urls.python_backend + "/get_asset_holders?asset_id=" + name)
                                 .then(function(response_ah) {
-                                    //console.log(response.data);
                                     angular.forEach(response_ah.data, function(value, key) {
-                                        //console.log(value);
-                                        //console.log(response.data[0].precision);
                                         var parsed = {name: value.name, amount: utilities.formatBalance(value.amount, response.data[0].precision), id: value.account_id};
                                         accounts.push(parsed);
                                     });
@@ -135,66 +124,13 @@
                     });
             }
 
-            // table 1
-            // column to sort
-            $scope.column = 'volume';
-            // sort ordering (Ascending or Descending). Set true for desending
-            $scope.reverse = true;
-            // called on header click
-            $scope.sortColumn = function(col){
-                $scope.column = col;
-                if($scope.reverse){
-                    $scope.reverse = false;
-                    $scope.reverseclass = 'arrow-up';
-                }else{
-                    $scope.reverse = true;
-                    $scope.reverseclass = 'arrow-down';
-                }
-            };
-            // remove and change class
-            $scope.sortClass = function(col) {
-                if ($scope.column == col) {
-                    if ($scope.reverse) {
-                        return 'arrow-down';
-                    } else {
-                        return 'arrow-up';
-                    }
-                } else {
-                    return '';
-                }
-            }
-            // table 2
-            // column to sort
-            $scope.column2 = 'amount';
-            // sort ordering (Ascending or Descending). Set true for desending
-            $scope.reverse2 = true;
-            // called on header click
-            $scope.sortColumn2 = function(col2){
-                $scope.column2 = col2;
-                if($scope.reverse2){
-                    $scope.reverse2 = false;
-                    $scope.reverseclass2 = 'arrow-up';
-                }else{
-                    $scope.reverse2 = true;
-                    $scope.reverseclass2 = 'arrow-down';
-                }
-            };
-            // remove and change class
-            $scope.sortClass2 = function(col2) {
-                if ($scope.column2 == col2) {
-                    if ($scope.reverse2) {
-                        return 'arrow-down';
-                    } else {
-                        return 'arrow-up';
-                    }
-                } else {
-                    return '';
-                }
-            }
+            utilities.columnsort($scope, "volume", "sortColumn", "sortClass", "reverse", "reverseclass", "column");
+            utilities.columnsort($scope, "amount", "sortColumn2", "sortClass2", "reverse2", "reverseclass2", "column2");
+
 		}
 		else {
 			var init;
-            if(path == "/assets") {
+            if(path === "/assets") {
 
                 $http.get(appConfig.urls.python_backend + "/get_dex_total_volume")
                     .then(function(response) {
@@ -209,7 +145,6 @@
                 $scope.dex_volume_chart = {};
                 $http.get(appConfig.urls.python_backend + "/daily_volume_dex_dates")
                     .then(function(response) {
-                        //console.log(response.data);
                         $http.get(appConfig.urls.python_backend + "/daily_volume_dex_data")
                             .then(function(response2) {
 
@@ -221,9 +156,6 @@
                                     tooltip : {
                                         trigger: 'axis'
                                     },
-                                    //legend: {
-                                        //data:['Volume']
-                                    //},
                                     toolbox: {
                                         show : true,
                                         feature : {
@@ -233,10 +165,7 @@
 
                                     xAxis : [
                                         {
-                                            //type : 'category',
                                             boundaryGap : true,
-                                            //axisTick: {onGap:false},
-                                            //splitLine: {show:false},
                                             data : response.data
                                         }
                                     ],
@@ -273,27 +202,25 @@
 
                 $http.get(appConfig.urls.python_backend + "/assets")
                     .then(function(response) {
-                        //console.log(response.data);
                         var assets = [];
                         var name = "";
                         angular.forEach(response.data, function(value, key) {
                             var market_cap;
                             var supply;
                             if(value[1].indexOf("OPEN") >= 0 || value[1].indexOf("RUDEX") >= 0) {
-                                var market_cap = "-";
-                                var supply = "-";
+                                market_cap = "-";
+                                supply = "-";
                             }
                             else {
-                                var market_cap = Math.round(value[5]/100000); // in bts always by now
+                                market_cap = Math.round(value[5]/100000); // in bts always by now
                                 var precision = 100000;
-                                if(value[10])
+                                if(value[10]) {
                                     precision = Math.pow(10, value[10]);
-                                var supply = Math.round(value[7]/precision);
+                                }
+                                supply = Math.round(value[7]/precision);
                             }
                             var volume = Math.round(value[4]);
                             var name_lower = value[1].replace("OPEN.", "").toLowerCase();
-
-                            //var image_url = "images/asset-symbols/"+name_lower+".png";
                             var url = "images/asset-symbols/"+name_lower+".png";
                             var image_url = "";
 
@@ -303,13 +230,14 @@
                             }).then(function successCallback(response) {
                                 image_url = "images/asset-symbols/"+name_lower+".png";
                                 var parsed = {name: value[1], image_url: image_url, id: value[2], price: value[3], volume: volume, type: value[6], market_cap: market_cap, supply: supply, holders: value[8]};
-
                                 assets.push(parsed);
                             }, function errorCallback(response) {
-                                if(type == "SmartCoin")
+                                if(type === "SmartCoin") {
                                     image_url = "images/asset-symbols/white.png";
-                                else
+                                }
+                                else {
                                     image_url = "images/asset-symbols/white.png";
+                                }
                                 var parsed = {name: value[1], image_url: image_url, id: value[2], price: value[3], volume: parseInt(volume), type: value[6], market_cap: market_cap, supply: supply, holders: value[8]};
                                 assets.push(parsed);
                             });
@@ -317,33 +245,8 @@
                         $scope.assets = assets;
                     });
 			}
-            // column to sort
-            $scope.column = 'volume';
-            // sort ordering (Ascending or Descending). Set true for desending
-            $scope.reverse = true;
-            // called on header click
-            $scope.sortColumn = function(col){
-                $scope.column = col;
-                if($scope.reverse){
-                    $scope.reverse = false;
-                    $scope.reverseclass = 'arrow-up';
-                }else{
-                    $scope.reverse = true;
-                    $scope.reverseclass = 'arrow-down';
-                }
-            };
-            // remove and change class
-            $scope.sortClass = function(col) {
-                if ($scope.column == col) {
-                    if ($scope.reverse) {
-                        return 'arrow-down';
-                    } else {
-                        return 'arrow-up';
-                    }
-                } else {
-                    return '';
-                }
-            }
+
+            utilities.columnsort($scope, "volume", "sortColumn", "sortClass", "reverse", "reverseclass", "column");
 		}
     }
 
--- app/sections/committee_members/committee_members.controller.js
+++ app/sections/committee_members/committee_members.controller.js
@@ -8,83 +8,28 @@
 
         $http.get(appConfig.urls.python_backend + "/get_committee_members")
             .then(function(response) {
-                //console.log(response.data);
                 var active_committee = [];
                 var standby_committee = [];
                 var counter = 1;
                 angular.forEach(response.data, function(value, key) {
-                    //console.log(value[0].id);
-                    var parsed = {id: value[0].id, total_votes: utilities.formatBalance(value[0].total_votes, 5), url: value[0].url,
-                        committee_member_account: value[0].committee_member_account, committee_member_account_name: value[0].committee_member_account_name, counter: counter};
-
-                    if(counter <= 11)
+                    var parsed = { id: value[0].id, total_votes: utilities.formatBalance(value[0].total_votes, 5),
+                                   url: value[0].url, committee_member_account: value[0].committee_member_account,
+                                   committee_member_account_name: value[0].committee_member_account_name, counter: counter
+                    };
+                    if(counter <= 11) {
                         active_committee.push(parsed);
-                    else
+                    }
+                    else {
                         standby_committee.push(parsed);
-
+                    }
                     counter++;
                 });
                 $scope.active_committee = active_committee;
                 $scope.standby_committee = standby_committee;
             });
 
-        // table 1
-        // column to sort
-        $scope.column = 'counter';
-        // sort ordering (Ascending or Descending). Set true for desending
-        $scope.reverse = false;
-        // called on header click
-        $scope.sortColumn = function(col){
-            $scope.column = col;
-            if($scope.reverse){
-                $scope.reverse = false;
-                $scope.reverseclass = 'arrow-up';
-            }else{
-                $scope.reverse = true;
-                $scope.reverseclass = 'arrow-down';
-            }
-        };
-        // remove and change class
-        $scope.sortClass = function(col) {
-            if ($scope.column == col) {
-                if ($scope.reverse) {
-                    return 'arrow-down';
-                } else {
-                    return 'arrow-up';
-                }
-            } else {
-                return '';
-            }
-        }
-        // table 2
-        // column to sort
-        $scope.column2 = 'counter';
-        // sort ordering (Ascending or Descending). Set true for desending
-        $scope.reverse2 = false;
-        // called on header click
-        $scope.sortColumn2 = function(col2){
-            $scope.column2 = col2;
-            if($scope.reverse2){
-                $scope.reverse2 = false;
-                $scope.reverseclass2 = 'arrow-up';
-            }else{
-                $scope.reverse2 = true;
-                $scope.reverseclass2 = 'arrow-down';
-            }
-        };
-        // remove and change class
-        $scope.sortClass2 = function(col2) {
-            if ($scope.column2 == col2) {
-                if ($scope.reverse2) {
-                    return 'arrow-down';
-                } else {
-                    return 'arrow-up';
-                }
-            } else {
-                return '';
-            }
-        }
+        utilities.columnsort($scope, "total_votes", "sortColumn", "sortClass", "reverse", "reverseclass", "column");
+        utilities.columnsort($scope, "total_votes", "sortColumn2", "sortClass2", "reverse2", "reverseclass2", "column2");
 
     }
-
 })();
