8210da3e46cb502c43fac2f4c2cd9d757f7df4fb
oxarbitrage <oxarbitrage@gmail.com>
2018-08-05 11:10:02 -0700

    move transaction business logic to service

--- app/core/service.network.js
+++ app/core/service.network.js
@@ -44,62 +44,94 @@
                 });
             },
             getLastOperations: function(callback) {
-                $http.get(appConfig.urls.elasticsearch_wrapper + "/get_account_history?from_date=now-1w&to_date=now&size=20")
+                $http.get(appConfig.urls.elasticsearch_wrapper + "/get_account_history?from_date=now-1h&to_date=now&size=20")
                     .then(function (response) {
 
-                        console.log(response.data);
-                        var lastops = [];
-                        angular.forEach(response.data, function (value, key) {
+                    var lastops = [];
+                    angular.forEach(response.data, function (value, key) {
 
-                            var operation = {};
-                            operation.block_num = value.block_data.block_num;
-                            operation.operation_id = value.account_history.operation_id;
-                            operation.time = value.block_data.block_time;
+                        var operation = {};
+                        operation.block_num = value.block_data.block_num;
+                        operation.operation_id = value.account_history.operation_id;
+                        operation.time = value.block_data.block_time;
 
-                            var parsed_op = JSON.parse(value.operation_history.op);
+                        var parsed_op = JSON.parse(value.operation_history.op);
 
-                            utilities.opText(appConfig, $http, value.operation_type, parsed_op[1], function(returnData) {
-                                operation.operation_text = returnData;
-                            });
-
-                            var type_res =  utilities.operationType(value.operation_type);
-                            operation.type = type_res[0];
-                            operation.color = type_res[1];
-
-
-                            lastops.push(operation);
-                            /*
-                            parsed.operation_id = value.;
-                            parsed.account = value[7];
-                            parsed.account_name = value[8];
-
-                            var type_res =  utilities.operationType(value[9]);
-                            parsed.type = type_res[0];
-                            parsed.color = type_res[1];
-
-                            parsed.id = value[1];
-                            var time = new Date(value[6]);
-                            parsed.time = time.toLocaleString();
-
-                            var operation_type = value[9];
-                            var operation = value[11];
-
-                            var operation_text = "";
-                            operation_text = utilities.opText(appConfig, $http, operation_type, operation, function(returnData) {
-                                parsed.operation_text = returnData;
-                            });
-
-                            last10.push(parsed);
-                            $scope.operations = last10;
-                            */
+                        utilities.opText(appConfig, $http, value.operation_type, parsed_op[1], function(returnData) {
+                            operation.operation_text = returnData;
                         });
-                        callback(lastops);
-                    });
-            }
 
+                        var type_res =  utilities.operationType(value.operation_type);
+                        operation.type = type_res[0];
+                        operation.color = type_res[1];
+
+                        lastops.push(operation);
+
+                    });
+                    callback(lastops);
+                });
+            },
+
+            getBigTransactions: function(callback) {
+                $http.get(appConfig.urls.elasticsearch_wrapper + "/get_account_history?from_date=now-1h&to_date=now&type=aggs&agg_field=block_data.trx_id.keyword&size=20")
+                    .then(function (response) {
+
+                    var transactions = [];
+                    angular.forEach(response.data, function (value, key) {
+                        if(value.key !== "") {
+                            var parsed = {
+                                trx_id: value.key,
+                                count: value.doc_count
+                            };
+                            transactions.push(parsed);
+                        }
+                    });
+                    callback(transactions);
+                });
+            },
+
+            getTransactionMetaData: function(trx, callback) {
+                var data;
+                $http.get(appConfig.urls.elasticsearch_wrapper + "/get_trx?trx=" + trx + "&size=1&sort=-operation_history.sequence")
+                    .then(function(response) {
+
+                    data = {
+                        hash: response.data[0].block_data.trx_id,
+                        counter: response.data[0].operation_history.op_in_trx,
+                        block_num: response.data[0].block_data.block_num,
+                        date: response.data[0].block_data.block_time
+                    };
+                    callback(data);
+                });
+            },
+
+            getTransactionOperations: function(trx, callback) {
+                var data;
+                $http.get(appConfig.urls.elasticsearch_wrapper + "/get_trx?trx=" + trx + "&size=100&sort=-operation_history.sequence")
+                    .then(function(response) {
+
+                    var operations = [];
+                    angular.forEach(response.data, function (value, key) {
+                        var op = utilities.operationType(value.operation_type);
+                        var op_type = op[0];
+                        var op_color = op[1];
+
+                        var parsed = {
+                            operation_id: value.account_history.operation_id,
+                            op_color: op_color,
+                            op_type: op_type
+                        };
+
+                        var opArray = JSON.parse(value.operation_history.op);
+                        utilities.opText(appConfig, $http, opArray[0], opArray[1], function (returnData) {
+                            parsed.operation_text = returnData;
+                        });
+                        operations.push(parsed);
+                    });
+                    callback(operations);
+                });
+            }
         };
     }
-
-
 
 })();
--- app/sections/txs/txs.controller.js
+++ app/sections/txs/txs.controller.js
@@ -2,59 +2,30 @@
     'use strict';
 
     angular.module('app.txs')
-        .controller('txsCtrl', ['$scope', '$filter', '$routeParams', '$location', '$http', 'appConfig', 'utilities', txsCtrl]);
+        .controller('txsCtrl', ['$scope', '$routeParams', '$location', 'networkService', txsCtrl]);
 
-    function txsCtrl($scope, $filter, $routeParams, $location, $http, appConfig, utilities) {
+    function txsCtrl($scope, $routeParams, $location, networkService) {
 
         var path = $location.path();
         var name = $routeParams.name;
 
         if(name) {
             if (path.includes("txs")) {
-                $http.get(appConfig.urls.elasticsearch_wrapper + "/get_trx?trx=" + name + "&size=1000")
-                    .then(function(response) {
 
-                        var operations = [];
+                networkService.getTransactionMetaData(name, function (returnData) {
+                    $scope.data = returnData;
+                });
 
-                        $scope.data = {name: name, counter: response.data.length, block_num: response.data[0].block_data.block_num,
-                        date: response.data[0].block_data.block_time};
-
-                        angular.forEach(response.data, function (value, key) {
-                            var op =  utilities.operationType(value.operation_type);
-                            var op_type = op[0];
-                            var op_color = op[1];
-
-                            var parsed = {operation_id: value.account_history.operation_id, op_color: op_color, op_type: op_type};
-
-                            var opArray = JSON.parse(value.operation_history.op);
-                            var operation_text = "";
-                            operation_text = utilities.opText(appConfig, $http, opArray[0],opArray[1], function(returnData) {
-                                parsed.operation_text = returnData;
-                            });
-                            operations.push(parsed);
-                        });
-                        $scope.operations = operations;
-                    });
-
+                networkService.getTransactionOperations(name, function (returnData) {
+                    $scope.operations = returnData;
+                });
             }
         }
         else {
-            if (path == "/txs") {
-
-                $http.get(appConfig.urls.elasticsearch_wrapper + "/get_account_history?from_date=now-1h&to_date=now&type=aggs&agg_field=block_data.trx_id.keyword")
-                    .then(function (response) {
-
-                        //console.log(response.data);
-                        var transactions = [];
-                        angular.forEach(response.data, function (value, key) {
-                            //console.log(value);
-                            var parsed = {trx_id: value.key, count: value.doc_count};
-                            //if(counter <= 10)
-                            transactions.push(parsed);
-                        });
-                        $scope.transactions = transactions;
-                    });
-
+            if (path === "/txs") {
+                networkService.getBigTransactions(function (returnData) {
+                    $scope.transactions = returnData;
+                });
             }
         }
     }
