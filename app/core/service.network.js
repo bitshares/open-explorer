63e8b5f90e2a8fa883cf039edbc7594117fc0af2
oxarbitrage <oxarbitrage@gmail.com>
2018-08-05 13:21:58 -0700

    move fee page business logic to service

--- app/core/service.network.js
+++ app/core/service.network.js
@@ -130,6 +130,35 @@
                     });
                     callback(operations);
                 });
+            },
+            getFees: function(callback) {
+                var fees = [];
+                $http.get(appConfig.urls.python_backend + "/fees").then(function(response) {
+                    var basic_fee = 0;
+                    //var premium_fee = 0;
+                    //var price_per_kbyte = 0;
+                    for(var i = 0; i < response.data.parameters.current_fees.parameters.length; i++) {
+                        if (response.data.parameters.current_fees.parameters[i][1].fee) {
+                            basic_fee = response.data.parameters.current_fees.parameters[i][1].fee;
+                        }
+                        else {
+                            basic_fee = response.data.parameters.current_fees.parameters[i][1].basic_fee;
+                        }
+                        var op_type  = utilities.operationType(response.data.parameters.current_fees.parameters[i][0]);
+
+                        var fee = {
+                            identifier: response.data.parameters.current_fees.parameters[i][0],
+                            operation: op_type[0],
+                            color: op_type[1],
+                            basic_fee: utilities.formatBalance(basic_fee, 5),
+                            premium_fee: utilities.formatBalance(response.data.parameters.current_fees.parameters[i][1].premium_fee, 5),
+                            price_per_kbyte: utilities.formatBalance(response.data.parameters.current_fees.parameters[i][1].price_per_kbyte, 5)
+                        };
+                        fees.push(fee);
+                    }
+                });
+                callback(fees);
+
             }
         };
     }
