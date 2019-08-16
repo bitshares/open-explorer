12aa73688c3943a85d63e7e2201d9006a4f9cbe4
Alfredo <oxarbitrage@gmail.com>
2019-08-16 19:32:42 -0300

    fix wrong indentation

--- app/sections/operations/operations.controller.js
+++ app/sections/operations/operations.controller.js
@@ -10,29 +10,27 @@
         var name = $routeParams.name;
 
         if(name) {
-            if(path.includes("operations")) {
+            if (path.includes("operations")) {
                 networkService.getOperation(name, function (returnData) {
                     $scope.data = returnData;
                 });
             }
+        }
+        else {
+            if (path === "/operations") {
+                $scope.select = function (page_operations) {
+                    var page = page_operations - 1;
+                    var limit = 50;
+                    var from = page * limit;
 
+                    networkService.getLastOperations(limit, from, function (returnData) {
+                        $scope.operations = returnData;
+                        $scope.currentPage = page_operations;
+                        $scope.total_ops = 10000;
+                    });
+                };
+                $scope.select(1);
             }
-            else {
-                if(path === "/operations") {
-                    $scope.select = function(page_operations) {
-                        var page = page_operations -1;
-                        var limit = 50;
-                        var from = page * limit;
-
-                        networkService.getLastOperations(limit, from, function (returnData) {
-                            $scope.operations = returnData;
-                            $scope.currentPage = page_operations;
-                            $scope.total_ops = 10000;
-                        });
-                    };
-                    $scope.select(1);
-                }
-            }
-
+        }
     }
 })();
