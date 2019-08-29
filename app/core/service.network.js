bf1d6aca107cf4a69daca03c226b7d60d1e55ed7
Alfredo <oxarbitrage@gmail.com>
2019-08-29 12:34:44 -0300

    fix typo, cleanup network service

--- app/core/service.network.js
+++ app/core/service.network.js
@@ -8,7 +8,7 @@
 
         return {
             getHeader: function(callback) {
-                var header;
+                let header;
                 $http.get(appConfig.urls.python_backend + "/header").then(function(response) {
 
                     header = {
@@ -17,7 +17,7 @@
                         bts_market_cap: response.data.bts_market_cap,
                         quote_volume: response.data.quote_volume,
                         witness_count: response.data.witness_count,
-                        committee_count: response.data.commitee_count
+                        committee_count: response.data.committee_count
                     };
                     callback(header);
                 });
@@ -28,11 +28,11 @@
                     "/es/account_history?from_date=now-1w&to_date=now&type=aggs&agg_field=block_data.block_num&size=20")
                     .then(function (response) {
 
-                    var blocks = [];
-                    angular.forEach(response.data, function (value, key) {
+                    let blocks = [];
+                    angular.forEach(response.data, function (value) {
                         $http.get(appConfig.urls.python_backend + "/block?block_num=" + value.key).then(function (response) {
 
-                            var parsed = {
+                            const parsed = {
                                 block_num: value.key,
                                 operations: value.doc_count,
                                 transactions: response.data.transactions.length,
@@ -49,29 +49,28 @@
                     "/es/account_history?size=" + limit + "&from_=" + from + "&from_date=now-1d")
                     .then(function (response) {
 
-                    var lastops = [];
-                    angular.forEach(response.data, function (value, key) {
+                    let last_ops = [];
+                    angular.forEach(response.data, function (value) {
 
-                        var operation = {};
+                        let operation = {};
                         operation.block_num = value.block_data.block_num;
                         operation.operation_id = value.account_history.operation_id;
                         operation.time = value.block_data.block_time;
 
-                        //var parsed_op = JSON.parse(value.operation_history.op);
-                        var parsed_op = value.operation_history.op_object;
+                        const parsed_op = value.operation_history.op_object;
 
                         utilities.opText(appConfig, $http, value.operation_type, parsed_op, function(returnData) {
                             operation.operation_text = returnData;
                         });
 
-                        var type_res =  utilities.operationType(value.operation_type);
+                        const type_res =  utilities.operationType(value.operation_type);
                         operation.type = type_res[0];
                         operation.color = type_res[1];
 
-                        lastops.push(operation);
+                        last_ops.push(operation);
 
                     });
-                    callback(lastops);
+                    callback(last_ops);
                 });
             },
 
@@ -80,10 +79,10 @@
                     "/es/account_history?from_date=now-1h&to_date=now&type=aggs&agg_field=block_data.trx_id.keyword&size=20")
                     .then(function (response) {
 
-                    var transactions = [];
-                    angular.forEach(response.data, function (value, key) {
+                    let transactions = [];
+                    angular.forEach(response.data, function (value) {
                         if(value.key !== "") {
-                            var parsed = {
+                            const parsed = {
                                 trx_id: value.key,
                                 count: value.doc_count
                             };
@@ -95,7 +94,7 @@
             },
 
             getTransactionMetaData: function(trx, callback) {
-                var data;
+                let data;
                 $http.get(appConfig.urls.elasticsearch_wrapper + "/es/trx?trx=" + trx +
                     "&size=1&sort=-operation_history.sequence")
                     .then(function(response) {
@@ -111,24 +110,23 @@
             },
 
             getTransactionOperations: function(trx, callback) {
-                var data;
                 $http.get(appConfig.urls.elasticsearch_wrapper + "/es/trx?trx=" + trx +
                     "&size=100&sort=-operation_history.sequence")
                     .then(function(response) {
 
-                    var operations = [];
-                    angular.forEach(response.data, function (value, key) {
-                        var op = utilities.operationType(value.operation_type);
-                        var op_type = op[0];
-                        var op_color = op[1];
+                    let operations = [];
+                    angular.forEach(response.data, function (value) {
+                        const op = utilities.operationType(value.operation_type);
+                        const op_type = op[0];
+                        const op_color = op[1];
 
-                        var parsed = {
+                        const parsed = {
                             operation_id: value.account_history.operation_id,
                             op_color: op_color,
                             op_type: op_type
                         };
 
-                        var opArray = value.operation_history.op_object;
+                        const opArray = value.operation_history.op_object;
                         utilities.opText(appConfig, $http, value.operation_type, opArray, function (returnData) {
                             parsed.operation_text = returnData;
                         });
@@ -138,11 +136,9 @@
                 });
             },
             getFees: function(callback) {
-                var fees = [];
+                let fees = [];
                 $http.get(appConfig.urls.python_backend + "/fees").then(function(response) {
-                    var basic_fee = 0;
-                    //var premium_fee = 0;
-                    //var price_per_kbyte = 0;
+                    let basic_fee = 0;
                     for(var i = 0; i < response.data.parameters.current_fees.parameters.length; i++) {
                         if (response.data.parameters.current_fees.parameters[i][1].fee) {
                             basic_fee = response.data.parameters.current_fees.parameters[i][1].fee;
@@ -152,7 +148,7 @@
                         }
                         var op_type  = utilities.operationType(response.data.parameters.current_fees.parameters[i][0]);
 
-                        var fee = {
+                        const fee = {
                             identifier: response.data.parameters.current_fees.parameters[i][0],
                             operation: op_type[0],
                             color: op_type[1],
@@ -166,10 +162,10 @@
                 callback(fees);
             },
             getOperation: function(operation, callback) {
-                var op;
+                let op;
                 $http.get(appConfig.urls.python_backend + "/operation?operation_id=" + operation).then(function(response) {
-                    var raw_obj = response.data.op;
-                    var op_type =  utilities.operationType(response.data.op_type);
+                    const raw_obj = response.data.op;
+                    const op_type =  utilities.operationType(response.data.op_type);
 
                     utilities.opText(appConfig, $http, response.data.op_type, raw_obj, function(returnData) {
                         op = {
@@ -191,9 +187,9 @@
                 });
             },
             getBlock: function(block_num, callback) {
-                var block;
+                let block;
                 $http.get(appConfig.urls.python_backend + "/block?block_num=" + block_num).then(function (response) {
-                    var operations_count = 0;
+                    let operations_count = 0;
                     for (var i = 0; i < response.data.transactions.length; i++) {
                         operations_count = operations_count + response.data.transactions[i].operations.length;
                     }
@@ -215,10 +211,10 @@
             },
             getObject: function(object, callback) {
                 $http.get(appConfig.urls.python_backend + "/object?object=" + object).then(function(response) {
-                    var object_id = response.data.id;
-                    var object_type = utilities.objectType(object_id);
+                    const object_id = response.data.id;
+                    const object_type = utilities.objectType(object_id);
 
-                    var object = {
+                    const object = {
                         raw: response.data,
                         name: object_id,
                         type: object_type
